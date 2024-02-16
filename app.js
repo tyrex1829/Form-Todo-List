08776c95b41ba89c8453ecd1a26fdad6d25e75ff
Saksham Khare <sakshamkhare19@gmail.com>
2024-02-16 16:12:21 +0530

    update all files, modified this app

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const form = document.querySelector("#shelterForm");
-const input = document.querySelector("#itemName");
-const ul = document.querySelector("#item");
+const ul = document.querySelector("#items");
 
 form.addEventListener("submit", function(e){
     e.preventDefault();
-    const li = document.createElement("li");
-    li.textContent = input.value;
-    ul.append(li);
-    input.value = "";
+
+    const itemnameInput = form.elements.itemname;
+    const descriptionInput = form.elements.description;
+
+    addTodoList(itemnameInput.value, descriptionInput.value);
+    
+    itemnameInput.value = "";
+    descriptionInput.value = "";
 })
+
+const addTodoList = (itemname, description) => {
+    const li = document.createElement("li");
+    const bTag = document.createElement("b");
+
+    bTag.append(itemname);
+    li.append(bTag);
+    li.append(` - ${description}`);
+    ul.append(li);
+}
